386d6ef9903856f3b8b85c761c4318ff0285b4d3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-09 19:42:53 -0500

    :building_construction: Basic Structure

---
+++ colores/__init__.py
@@ -0,0 +1,12 @@
+"""
+This file defines colores as a module.
+"""
+
+from .core import *
+
+# Package Meta Data
+__version__ = "0.1.0"
+__authors__ = ["Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>"]
+__author_email__ = "eliaz.bobadilladev@gmail.com"
+__url__ = "https://github.com/UltiRequiem/colores"
+__package_name__ = "colores"
